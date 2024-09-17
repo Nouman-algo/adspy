179394a22628602fd66e248bfed991662c134f12
Bilal-ahmad367 <bilal.ahmad@optimusalgos.com>
2024-09-17 00:11:08 -0700

    dashboard design

---
+++ app/browser-add/page.tsx
@@ -0,0 +1,11 @@
+import BrowseAdsAndReviews from '@/components/BrowseAdsAndReviews'
+
+const Home = () => {
+  return (
+    <div>
+      <BrowseAdsAndReviews />
+    </div>
+  )
+}
+
+export default Home
