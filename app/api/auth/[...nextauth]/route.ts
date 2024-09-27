92e0350475941831b82d2e1a7843ec8d3a60fb0b
Nouman-algo <nouman.khan@optimusalgos.com>
2024-09-27 22:30:48 +0500

    updated

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,17 +1,19 @@
-import NextAuth from 'next-auth/next'
-import GoogleProvider from 'next-auth/providers/google'
+import NextAuth from 'next-auth';
+import GoogleProvider from 'next-auth/providers/google';
 
+export const authOptions = {
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID!,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+    }),
+  ],
+  // Uncomment the following line if you want to specify a secret
+  // secret: process.env.JWT_SECRET,
+};
 
-export const authOptions = ({
-    providers :[
-        GoogleProvider({
-            clientId:process.env.GOOGLE_CLIENT_ID!,
-            clientSecret:process.env.GOOGLE_CLIENT_SECRET!
-        })
-    ],
-    // secret: process.env.JWT_SECRET
-})
+// Use NextAuth with the authOptions
+const handler = NextAuth(authOptions);
 
-const handler = NextAuth(authOptions)
-export {handler as GET , handler as POST}
-
+// Export handlers for GET and POST requests
+export { handler as GET, handler as POST };
